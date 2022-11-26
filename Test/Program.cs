bf5bdc079ca6e45575269633bbe162e9c2f0770e
Victoria <victoria.kainova@gmail.com>
2022-11-26 18:33:43 +0300

    Added program structure

---
+++ Test/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+
+Console.Clear();
+
+string[] array1 = new string[10] {"hello","2","world","my","list","for","who","computer","8765","of"};
+string[] array2 = FillSecondArray(array1);
+
+PrintArray(array1);
+Console.WriteLine();
+PrintArray(array2);
